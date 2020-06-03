287eb6027d1e72d947c6b10a2772c2895a741f53
anlen123 <1761512493@qq.com>
2020-06-03 18:07:22 +0800

    每日100张色图

---
+++ auto_push.sh
@@ -0,0 +1,7 @@
+python sex.py
+
+git add *
+git add -A 
+git commit -m "每日100张色图"
+git status
+git push
--- sex.py
+++ sex.py
@@ -7,13 +7,14 @@ import os
 try:
     if os.path.exists("README.md"):
         os.system("rm -rf  README.md")
+    with open("README.md", "a+")as f:
+	f.write("# SexDay\n")
 
     for _ in range(20):
         sql = "select download_url from urls where id ={};".format(random.randint(1, 240000))
         cur.execute(sql)
         res = cur.fetchall()
         with open("README.md", "a+")as f:
-	        f.write("# SexDay")
             for x in res:
                 # print(x['download_url'])
                 f.write("![]({})\n".format(x['download_url']))
