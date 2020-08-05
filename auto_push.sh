5f85466e30cb7d6d705c3df3c19611d4124ddbd8
anlen123 <1761512493@qq.com>
2020-08-05 11:14:07 +0800

    每日100张色图

--- auto_push.sh
+++ auto_push.sh
@@ -2,8 +2,8 @@
 cd /root/SexDay
 python3 sex.py
 
-/usr/git/bin/git add *
-/usr/git/bin/git add -A 
-/usr/git/bin/git commit -m "每日100张色图"
-/usr/git/bin/git status
-/usr/git/bin/git push origin master
+git add *
+git add -A 
+git commit -m "每日100张色图"
+git status
+git push 
