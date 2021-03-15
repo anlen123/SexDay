bc894f0148ddc1acf4b8fc6d849687e860bc415c
anlen123 <1761512493@qq.com>
2021-03-15 09:27:32 +0800

    2021-03-15

--- auto_push.sh
+++ auto_push.sh
@@ -2,9 +2,8 @@
 cd /root/SexDay
 /root/miniconda3/bin/python3 sex.py
 date=$(date +%Y-%m-%d)
-git add *
-git add -A 
+git add .
 git commit -m "$date"
 git status
-git push 
+git push  -f 
 touch flag
